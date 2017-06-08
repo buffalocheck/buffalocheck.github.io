c451eb065f2d2111b3b0b82e86855e6c2cc7571e
buffalocheck <shawn@buffalocheck.com>
2017-06-07 17:17:53 -0700

    fixed js bug

--- script.js
+++ script.js
@@ -10,16 +10,4 @@ $(document).ready(function() {
     $('.button-collapse').sideNav('destroy');
 
 
-    // $('a').on('click', function(event) {
-    //     event.preventDefault();
-    //     var target = $(this.hash);
-    //     $('html, body').animate({
-    //         scrollTop: target.offset().top
-    //     }, 1200);
-    //     return false;
-
-    });
-
-
-
 });
